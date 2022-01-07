34ad9c0169657e8745089079dd241ec33eb95161
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-07 19:05:47 +0700

    add custom controller

---
+++ application/controllers/Contoh.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Contoh extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Hallo ini adalah contoh controller</h1>";
+    }
+}
